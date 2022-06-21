c3891b1bd8ccecca3b23df3941b572db8c24cb1a
anzhy.diz <anzhy.nesty@gmail.com>
2022-06-20 22:33:40 -0400

    select image from gallery

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.drawingapp
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
@@ -14,6 +17,18 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackgroundView: ImageView = findViewById<ImageView>(R.id.iv_background)
+
+                imageBackgroundView.setImageURI(result.data?.data)
+            }
+        }
 
     private val multiplePermissionLauncher: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(
@@ -29,6 +44,12 @@ class MainActivity : AppCompatActivity() {
                         "Permission granted now you can read the storage files",
                         Toast.LENGTH_LONG
                     ).show()
+
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                    openGalleryLauncher.launch(pickIntent)
+
                 } else {
                     if (permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE) {
                         Toast.makeText(
@@ -56,9 +77,6 @@ class MainActivity : AppCompatActivity() {
 
      */
 
-
-    private var drawingView: DrawingView? = null
-    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
